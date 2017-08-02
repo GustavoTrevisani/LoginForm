f2447321e0676fd4bb68bb87b3387ec40f804b3c
Gustavo Trevisani <gu_trevisani@yahoo.com.br>
2017-08-02 17:32:53 -0300

    Subi o formulário, era erro nas dependências do Thymeleaf

--- src/main/java/com/loginform/controller/PageController.java
+++ src/main/java/com/loginform/controller/PageController.java
@@ -4,19 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
-
-
 @Controller
 public class PageController {
-	
+
 	@RequestMapping("/login")
-	public String form(Model model) {
-		System.out.println("teste");		
+	public String form() {
+		System.out.println("teste");
 		return "view/form";
-		
+
 	}
-	
-	
 
 }
